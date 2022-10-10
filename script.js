8101575e792a5360348e5a7703fcb9036bd2e4cf
RustemetsuR <ibraimov.rustem2003@gmail.com>
2022-10-10 15:56:38 +0600

    Added burger for header

--- script.js
+++ script.js
@@ -10,3 +10,17 @@ kgFlag.addEventListener("click", () =>{
     ruFlag.classList.remove("active");
     kgFlag.classList.toggle("active");
 });
+
+const hamburger = document.querySelector(".header__burger");
+const nav = document.querySelector(".header__nav");
+
+
+hamburger.addEventListener("click", () =>{
+    hamburger.classList.toggle("active");
+    nav.classList.toggle("active");
+});
+
+document.querySelectorAll(".header__nav-list-links").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    nav.classList.remove("active");
+}))
